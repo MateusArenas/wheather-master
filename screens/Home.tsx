3d1b400648c44509cc0e811500426219a9338fb0
Alan da Silva <alan.scaje@gmail.com>
2022-02-18 04:23:22 -0300

    alterei o background

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,12 +1,14 @@
+import { setStatusBarBackgroundColor } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet } from 'react-native' 
+import { withSafeAreaInsets } from 'react-native-safe-area-context';
 import { Text, View } from '../components/Themed';
 import { RootStackScreenProps } from '../types';
 
 export default function Home({ navigation, route }: RootStackScreenProps<'Home'>) {
     return (
         <View style={styles.container}>
-        <Text style={styles.title}>SOY OH MEJOR</Text>
+        <Text style={styles.title}>Weather Forecast</Text>
         <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       </View>
     );
@@ -17,11 +19,16 @@ const styles = StyleSheet.create({
       flex: 1,
       alignItems: 'center',
       justifyContent: 'center',
+      backgroundColor: '#d0efff',
     },
+
     title: {
-      fontSize: 20,
-      fontWeight: 'bold',
+      fontSize: 40,
+      fontWeight: 'bold', 
+      top: '0',
+      backgroundColor: '#ffffff',
     },
+
     separator: {
       marginVertical: 30,
       height: 1,
