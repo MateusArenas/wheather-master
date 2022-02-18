dbe9f04eeaa3ff60878329f40ec174d1891581de
MateusArenas <MateusArenas97@gmail.com>
2022-02-18 01:20:20 -0300

    adding news

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { StyleSheet } from 'react-native' 
+import { StyleSheet, Image } from 'react-native' 
 import { Text, View } from '../components/Themed';
 import { RootStackScreenProps } from '../types';
 import * as Location from 'expo-location';
 import api from '../services/api';
 
 function kelvinToCelsius (kelvin: number) {
-    return (kelvin-273.15)
+    return (kelvin-273.15).toFixed() + "°"
 }
 
 export default function Home({ navigation, route }: RootStackScreenProps<'Home'>) {
@@ -44,7 +44,12 @@ export default function Home({ navigation, route }: RootStackScreenProps<'Home'>
 
     return (
       <View style={styles.container}>
+        <Image source={{ uri: `http://openweathermap.org/img/wn/${data?.weather[0]?.icon}@2x.png` }} 
+            style={{ width: 24*2, height: 24*2 }}
+        />
         <Text style={styles.title}>{data?.name}</Text>
+        <Text style={styles.title}>{kelvinToCelsius(data?.main?.temp)}</Text>
+        <Text style={styles.title}>{data?.weather[0]?.description}</Text>
         <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       </View>
     );
