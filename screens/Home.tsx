cbccb30ab66cccc59343e3b416cde91bdd9dcdac
MateusArenas <MateusArenas97@gmail.com>
2022-02-18 00:58:48 -0300

    hooks add and api

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -2,11 +2,49 @@ import React from 'react';
 import { StyleSheet } from 'react-native' 
 import { Text, View } from '../components/Themed';
 import { RootStackScreenProps } from '../types';
+import * as Location from 'expo-location';
+import api from '../services/api';
+
+function kelvinToCelsius (kelvin: number) {
+    return (kelvin-273.15)
+}
 
 export default function Home({ navigation, route }: RootStackScreenProps<'Home'>) {
+    const [location, setLocation] = React.useState<Location.LocationObject>()
+    const [data, setData] = React.useState()
+
+    React.useEffect(() => {
+        (async () => {
+          let { status } = await Location.requestForegroundPermissionsAsync();
+          if (status !== 'granted') {
+            // setErrorMsg('Permission to access location was denied');
+            return;
+          }
+    
+          const location = await Location.getCurrentPositionAsync({});
+          setLocation(location);
+        })();
+      }, []);
+
+    React.useEffect(() => {
+        if (location) {
+            (async () => {
+                const response = await api.get('/weather', { params: {
+                    q: 'sao paulo,br', 
+                    lat: location?.coords.latitude, lon: location?.coords.longitude, 
+                    mode: 'JSON'
+                } })
+    
+                setData(response?.data)
+                console.log({ data: response?.data });
+                
+            })()
+        }
+    }, [location]) 
+
     return (
-        <View style={styles.container}>
-        <Text style={styles.title}>SOY OH MEJOR</Text>
+      <View style={styles.container}>
+        <Text style={styles.title}>{data?.name}</Text>
         <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       </View>
     );
